473e6f5e6e48b201361b2feb714aa28de6f4464b
Martin Sommer <martin@sommer.net>
2021-05-23 20:01:51 -0700

    Exposing outstanding balance for individual, and groups of payments.

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -6,11 +6,39 @@ class LoansController < ActionController::API
 
   #All loans
   def index
-    render json: Loan.all
+    loan_set = []
+
+    Loan.all.each do |loan|
+      loan_balance = balance(loan.id)
+
+      loan_set << [
+          loan_id: loan.id,
+          funded_amount: loan.funded_amount,
+          outstanding_balance: loan_balance
+      ]
+    end
+
+    render json: loan_set
   end
 
   #Individual loan
   def show
-    render json: Loan.find(params[:id])
+    loan_detail = Loan.find(params[:id])
+    loan_balance = balance(params[:id])
+
+    render json: {
+        loan_id: loan_detail.id,
+        funded_amount: loan_detail.funded_amount,
+        outstanding_balance: loan_balance
+    }
+  end
+
+  private
+
+  def balance(loan_id)
+    loan = Loan.where(id: loan_id).first
+    funded_amount = loan ? loan.funded_amount : 0
+    total_payments = Payment.where(loan_id: loan_id).sum(:payment_amount)
+    funded_amount - total_payments
   end
 end
